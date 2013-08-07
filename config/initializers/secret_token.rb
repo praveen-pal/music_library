1c6ee9d154174eb8c8773babc40cda8af5d5ec13
praveen pal <pal.praveen11@gmail.com>
2013-08-07 10:48:05 +0530

    music test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MusicLibrary::Application.config.secret_key_base = '787c814da56c5e895a2c51722510a11b2225762404016310ecde291e03046ae3bf7bdbd6a27c01ce0ac69d56b08876a5c7f9849d724bd1efa6771af3a8892659'
